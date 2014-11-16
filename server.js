d0be2b19843187600ca714f7ec0dcfa25fb6ef6e
Alex Knutson <alexknutson@gmail.com>
2014-11-16 15:35:13 -0600

    ok, so this kinda works, but I need to fix things so that the next request is made only AFTER the last one is completely written

--- server.js
+++ server.js
@@ -55,164 +55,272 @@ app.get('/scrapeUnits', function(req, res){
 
 app.get('/scrapeQuests', function(req, res){
 
-	url = 'http://bravefrontierglobal.wikia.com/wiki/Lizeria:Fire?action=render';
+	var quest_worlds = ['Mistral', 'Morgan', 'St._Lamia', 'Cordelica', 'Amdahl', 'Encervis', 'Palmyna', 'Lizeria', 'Ryvern', 'Agni', 'Mirvana', 'Lanara'];
 
-	request(url, function(error, response, html){
-		//console.log(response);
-		if(!error){
-			var $ = cheerio.load(html);
+	var quest_type = ['First', 'Fire', 'Water', 'Earth', 'Thunder', 'Light', 'Dark', 'Final', 'Bonus'];
+
+	var urls = [];
 
 
-			// Stats Header
-			var json = { name: "", levels: {} };
+	// Helper function to build a variety of URLs to hit with our scraper.
+	var urlBuilder = function(world, quest)	{
+		return 'http://bravefrontierglobal.wikia.com/wiki/' + world + ':' + quest + '?action=render';
+	}
+	
+	//urls.push(urlBuilder(world, type));
+	total_counter = 0;
 
-			var $table = $('.article-table');
 
-			// Quest Name
-			json.name = $table.find('span').first().text().trim();
+	// Default Validated is TRUE. Only if the response HTML doesnt have the right elements does this fail.
+	var url_validated = true;
 
-			var count = $table.children().length;
+	// Multiple Quests
+	quest_worlds.forEach(function(world){
+		quest_type.forEach(function(type){
+			// Build the URL we'll use for this scrape
+			var url = urlBuilder(world, type);
 
-			// Find each quest level and assign a class to identify it.
-			$table.children().each(function(index) {
+			// Default Validated is TRUE. Only if the response HTML doesnt have the right elements does this fail.
+			var url_validated = true;
 
-				if (index > 1) {
-					if ($(this).children().first().attr('scope') && $(this).children().first().text().trim() !== 'General Zone Details') {
-						$(this).addClass('level');
-						//console.log($($table).html());
+			request(url, function(error, response, html){
+				//console.log(response);
+				if(!error){
+
+					// Load the response into Cheerio so we can parse stuff.
+					var $ = cheerio.load(html);
+
+					// Stats Header
+					var json = { name: "", levels: {} };
+
+					// We store the article table.
+					var $table = $('.article-table');
+
+					// Quest Name
+					json.name = $table.find('span').first().text().trim();
+
+					// ## MAIN VALIDATOR. Checks to make sure the $table exists. If not, we'll skip the fs.write
+					if ($table.length === 0) {
+						console.log('No quest page to scrape. Skipping this document');
+						url_validated = false;
 					}
-				}
-				if (index === count -1) {
 
-					json.total_levels = $('.level').length;
+					// Genereral count.
+					var count = $table.children().length;
 
-					// We have all the levels. Lets add them to our JSON
-					$table.find('.level').each(function(index){
+					// Find each quest level and assign a class to identify it.
+					$table.children().each(function(index) {
 
-						// Store index so we can reference it when we create object properties
-						var level_index = index;
-						// Monsters column
-						var $monsters = $(this).next().find('td').eq(0);
-						// Notes column
-						var $notes = $(this).next().find('td').eq(1);
-
-						// Rare Captures
-						var $rare_captures = $(this).next().find('td').eq(2);
-						//console.log($notes.html());
-
-						//console.log($details.html());
-
-						// Level Schema
-						json.levels[index] = { 
-							name: $(this).children().eq(0).text().trim(), 
-							energy: parseInt($(this).children().eq(1).text().trim()), 
-							battles: parseInt($(this).children().eq(2).text().trim()), 
-							exp: parseInt($(this).children().eq(3).text().trim()), 
-							ratio: parseInt($(this).children().eq(4).text().trim()),
-						};
-						json.levels[index]['monsters'] = {};
-						json.levels[index]['notes'] = {};
-						json.levels[index]['rare_captures'] = {};
-						json.levels[index]['drops'] = {};
-
-						//console.log(json.levels);
-
-						// Assign monster data to the object
-						$monsters.find('b').each(function(index) {
-							console.log($(this).next().html());
-							json.levels[level_index]['monsters'][index] = {
-								name: $(this).text().trim(),
-								hp: parseInt($(this).next().text().trim().replace(/[^\d\.]/gi, ""), 10),
-							};
-						});
-
-						// Assign notes data to the object
-						$notes.find('p').each(function(index){
-							if ($(this).text().trim() === 'Notes:') {
-								//json.levels[level_index]['notes'][index] = $(this).text().trim();
-																$(this).next('ul').find('li').each(function(index){
-									json.levels[level_index]['notes'][index] = $(this).text().trim();
-								});
-							} else if ($(this).text().trim() === 'Drops:') {
-								$(this).next('ul').find('li').each(function(index){
-									json.levels[level_index]['drops'][index] = $(this).text().trim();
-								});
+						if (index > 1) {
+							if ($(this).children().first().attr('scope') && $(this).children().first().text().trim() !== 'General Zone Details') {
+								$(this).addClass('level');
+								//console.log($($table).html());
 							}
-						});
+						}
+						if (index === count -1) {
 
-						// Assign drops data to the object
-						//						$notes.find('ul').eq(1).find('li').each(function(index){
-						//							json.levels[level_index]['drops'][index] = $(this).text().trim();
-						//						});
+							json.total_levels = $('.level').length;
 
-						// Assign rare captures data to the object
-						$rare_captures.find('b').each(function(index) {
-							json.levels[level_index]['rare_captures'][index] = {
-								name: $(this).text().trim(),
-								odds: parseInt($(this).next().text().trim().match(/\d+/)),
-							};
-						});
+							// We have all the levels. Lets add them to our JSON
+							$table.find('.level').each(function(index){
 
+								// Store index so we can reference it when we create object properties
+								var level_index = index;
+								// Monsters column
+								var $monsters = $(this).next().find('td').eq(0);
+								// Notes column
+								var $notes = $(this).next().find('td').eq(1);
 
+								// Rare Captures
+								var $rare_captures = $(this).next().find('td').eq(2);
+								//console.log($notes.html());
 
+								//console.log($details.html());
 
+								// Level Schema
+								json.levels[index] = { 
+									name: $(this).children().eq(0).text().trim(), 
+									energy: parseInt($(this).children().eq(1).text().trim().replace(/[^\d\.]/gi, ""), 10), 
+									battles: parseInt($(this).children().eq(2).text().trim().replace(/[^\d\.]/gi, ""), 10), 
+									exp: parseInt($(this).children().eq(3).text().trim().replace(/[^\d\.]/gi, ""), 10), 
+									ratio: parseFloat($(this).children().eq(4).text().trim()),
+								};
+								json.levels[index]['monsters'] = {};
+								json.levels[index]['notes'] = {};
+								json.levels[index]['rare_captures'] = {};
+								json.levels[index]['drops'] = {};
 
-						// Monsters
-						//					var monsters = [];
-						//					//console.log($details.first('td').find('div b').text());
-						//						
-						//					json.levels[index]['monsters'] = {
-						//						names: $details.first('td').find('div b').first().text(),
-						//					};
+								//console.log(json.levels);
 
-						//						$(this).children().each(function(index){
-						//							//console.log(index);
-						//
-						//							switch(index) {
-						//								case 0:
-						//									json.levels[index].name = $(this).text().trim();
-						//									
-						//									//console.log($(this).html());
-						//									break;
-						//								case 1:
-						//											json.levels[index].name = $(this).text().trim();
-						//									break;
-						//								case 2:
-						//											json.levels[index].name = $(this).text().trim();
-						//									break;
-						//								case 3:
-						//											json.levels[index].name = $(this).text().trim();
-						//									break;
-						//
-						//							}
-						//							//console.log($(this).text());
-						//						});
+								// Assign monster data to the object
+								$monsters.find('b').each(function(index) {
+									//console.log($(this).next().html());
+									json.levels[level_index]['monsters'][index] = {
+										name: $(this).text().trim(),
+										hp: parseInt($(this).next().text().trim().replace(/[^\d\.]/gi, ""), 10),
+									};
+								});
 
+								// Assign notes data to the object
+								$notes.find('p').each(function(index){
+									if ($(this).text().trim() === 'Notes:') {
+										//json.levels[level_index]['notes'][index] = $(this).text().trim();
+										$(this).next('ul').find('li').each(function(index){
+											json.levels[level_index]['notes'][index] = $(this).text().trim();
+										});
+									} else if ($(this).text().trim() === 'Drops:') {
+										$(this).next('ul').find('a').each(function(index){
+											json.levels[level_index]['drops'][index] = $(this).text().trim();
+										});
+									}
+								});
 
+								// Assign rare captures data to the object
+								$rare_captures.find('b').each(function(index) {
+									json.levels[level_index]['rare_captures'][index] = {
+										name: $(this).text().trim(),
+										odds: parseInt($(this).next().text().trim().match(/\d+/)),
+									};
+								});
+							});	
+						}
 
-
-
-
-
-						//console.log($(this).html());
-					});	
+					});
+				}
+				if (url_validated == true) {
+					fs.writeFile('world/' + total_counter + '.json', JSON.stringify(json, null, 4), function(err){
+						console.log('File successfully written! - Check your project directory for the output.json file');
+						total_counter++;
+					})
 				}
 
-			});
+				//res.send(html)
+			})
 
-			//console.log($table.find('.level').html());
+		});
+	});
 
-			//console.log($table.find('th').first().html());
+
+	// Single quest
+//	url = 'http://bravefrontierglobal.wikia.com/wiki/Mirvana:Earth?action=render';
+//	request(url, function(error, response, html){
+//		//console.log(response);
+//		if(!error){
+//
+//			// Load the response into Cheerio so we can parse stuff.
+//			var $ = cheerio.load(html);
+//
+//			// Stats Header
+//			var json = { name: "", levels: {} };
+//
+//			// We store the article table.
+//			var $table = $('.article-table');
+//
+//			// Quest Name
+//			json.name = $table.find('span').first().text().trim();
+//
+//			// ## MAIN VALIDATOR. Checks to make sure the $table exists. If not, we'll skip the fs.write
+//			if ($table.length === 0) {
+//				console.log('No quest page to scrape. Skipping this document');
+//				url_validated = false;
+//			}
+//
+//			// Genereral count.
+//			var count = $table.children().length;
+//
+//			// Find each quest level and assign a class to identify it.
+//			$table.children().each(function(index) {
+//
+//				if (index > 1) {
+//					if ($(this).children().first().attr('scope') && $(this).children().first().text().trim() !== 'General Zone Details') {
+//						$(this).addClass('level');
+//						//console.log($($table).html());
+//					}
+//				}
+//				if (index === count -1) {
+//
+//					json.total_levels = $('.level').length;
+//
+//					// We have all the levels. Lets add them to our JSON
+//					$table.find('.level').each(function(index){
+//
+//						// Store index so we can reference it when we create object properties
+//						var level_index = index;
+//						// Monsters column
+//						var $monsters = $(this).next().find('td').eq(0);
+//						// Notes column
+//						var $notes = $(this).next().find('td').eq(1);
+//
+//						// Rare Captures
+//						var $rare_captures = $(this).next().find('td').eq(2);
+//						//console.log($notes.html());
+//
+//						//console.log($details.html());
+//
+//						// Level Schema
+//						json.levels[index] = { 
+//							name: $(this).children().eq(0).text().trim(), 
+//							energy: parseInt($(this).children().eq(1).text().trim().replace(/[^\d\.]/gi, ""), 10), 
+//							battles: parseInt($(this).children().eq(2).text().trim().replace(/[^\d\.]/gi, ""), 10), 
+//							exp: parseInt($(this).children().eq(3).text().trim().replace(/[^\d\.]/gi, ""), 10), 
+//							ratio: parseFloat($(this).children().eq(4).text().trim()),
+//						};
+//						json.levels[index]['monsters'] = {};
+//						json.levels[index]['notes'] = {};
+//						json.levels[index]['rare_captures'] = {};
+//						json.levels[index]['drops'] = {};
+//
+//						//console.log(json.levels);
+//
+//						// Assign monster data to the object
+//						$monsters.find('b').each(function(index) {
+//							//console.log($(this).next().html());
+//							json.levels[level_index]['monsters'][index] = {
+//								name: $(this).text().trim(),
+//								hp: parseInt($(this).next().text().trim().replace(/[^\d\.]/gi, ""), 10),
+//							};
+//						});
+//
+//						// Assign notes data to the object
+//						$notes.find('p').each(function(index){
+//							if ($(this).text().trim() === 'Notes:') {
+//								//json.levels[level_index]['notes'][index] = $(this).text().trim();
+//								$(this).next('ul').find('li').each(function(index){
+//									json.levels[level_index]['notes'][index] = $(this).text().trim();
+//								});
+//							} else if ($(this).text().trim() === 'Drops:') {
+//								$(this).next('ul').find('a').each(function(index){
+//									json.levels[level_index]['drops'][index] = $(this).text().trim();
+//								});
+//							}
+//						});
+//
+//						// Assign rare captures data to the object
+//						$rare_captures.find('b').each(function(index) {
+//							json.levels[level_index]['rare_captures'][index] = {
+//								name: $(this).text().trim(),
+//								odds: parseInt($(this).next().text().trim().match(/\d+/)),
+//							};
+//						});
+//					});	
+//				}
+//
+//			});
+//		}
+//		if (url_validated == true) {
+//			fs.writeFile('output-quests.json', JSON.stringify(json, null, 4), function(err){
+//				console.log('File successfully written! - Check your project directory for the output.json file');
+//				total_counter++;
+//			})
+//		}
+//
+//		res.send(html)
+//	})
+
+	//urls.push(urlBuilder(world, type));
+	//console.log(urls);
 
 
-		}
-
-		fs.writeFile('output-quests.json', JSON.stringify(json, null, 4), function(err){
-			console.log('File successfully written! - Check your project directory for the output.json file');
-		})
-
-		res.send(html)
-	})
 
 })
 
