cf74c54e065ba367bd4a975358fe72ba2291c9ec
Alex Knutson <alexknutson@gmail.com>
2014-12-23 09:53:57 -0600

    add route for 1 unit scrape only

--- server.js
+++ server.js
@@ -153,7 +153,8 @@ app.get('/scrapeUnits', function(req, res){
 							json.evo_5 = $(statsTable).find("th:contains('Cost')").siblings().find('a').eq(4).attr('title');
 
 							// Series (we use the first unit to decide what the name of the series is).
-							json.series = $(statsTable).find("th:contains('Chain')").siblings().find('a').eq(0).attr('title');
+							var series = $(statsTable).find("th:contains('Chain')").siblings().find('a').eq(0).attr('title');
+							json.series = decodeURI(series);
 
 							// How to Obtain
 							var obtainList = $('#unit-info-3');
@@ -183,7 +184,7 @@ app.get('/scrapeUnits', function(req, res){
 							if (json.sbb_skill == "")  { json.sbb_skill = 'None'; }
 		}
 
-					fs.writeFile('units/' + json.name + '.json', JSON.stringify(json, null, 4), function(err){
+					fs.writeFile('units/' + json.name.replace(/ /g,'') + '.json', JSON.stringify(json, null, 4), function(err){
 						console.log('File successfully written! - Check your project directory for the output.json file');
 					})
 
@@ -516,6 +517,178 @@ app.get('/scrapeQuests', function(req, res){
 
 
 });
+
+app.get('/scrapeOneUnit', function(req, res){
+	request('http://bravefrontierglobal.wikia.com/wiki/Dictator_Lilly_Matah', function(error, response, html){
+		//console.log(response);
+		if(!error){
+			var $ = cheerio.load(html);
+				
+							// HELPERS
+							var setUnitBBFill = function(div_text, row_type) {
+								if (row_type === 'bb') {
+									console.log('This is the BB row. Set the BB fill rate');
+									json.bb_fill = div_text;
+									delete row_type;
+								} else if (row_type === 'sbb') {
+									console.log('This is the BB row. Set the SBB fill rate');
+									// We need to do some more work to strip the undesired 'BC' text
+									json.sbb_fill = div_text;
+									delete row_type;
+								}
+							}
+							var getAbilityDetails = function (spanContainer) {
+								$(spanContainer).parent().siblings().find('span').each(function(){
+								var abilityTitle = $(this).attr('title');
+								$(this).after(' (' + abilityTitle + ')');
+								});
+							}
+				
+							// Stats Header
+							var json = { name : "", character_id : "", element : "", rarity : "", max_lvl : "", cost : "", gender: "", series: "", evo_1: "", evo_2: "", evo_3: "", evo_4: "", evo_5: "", evo_cost: "", hp_base: "", atk_base: "", def_base: "", rec_base: "", hp_lord: "", atk_lord: "", def_lord: "", rec_lord: "", hp_anima: "", atk_anima: "", def_anima: "", rec_anima: "", hp_breaker: "", atk_breaker: "", def_breaker: "", rec_breaker: "", hp_guardian: "", atk_guardian: "", def_guardian: "", rec_guardian: "", hp_oracle: "", atk_oracle: "", def_oracle: "", rec_oracle: "", leader_skill_name: "", leader_skill: "",  bb_skill_name: "", bb_skill: "", sbb_skill_name: "", sbb_skill: "", bb_fill: "", sbb_fill: "", bb_hits: "", sbb_hits: "", hits: "", icon: "", image: "", how_to_obtain: {}};
+				
+							var i=0;
+							var main_content = $("div [style*='max-width:900px']");
+
+							var test = $(main_content).children();
+							$("div [style*='max-width:900px']").children().each(function(index){
+								$(this).attr('id', 'unit-info-' + index);
+							});
+							//console.log($('#unit-info-0'));
+
+							// First wrapper that contains unit data (Name, Element, etc)
+							json.name = $('#unit-info-0').find('b').first().text().trim();
+							json.character_id = parseInt($('#unit-info-0').find("th:contains('No.')").next().text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.element = $('#unit-info-0').find("th:contains('Element')").next().text().trim();
+							json.gender = $('#unit-info-0').find("th:contains('Gender')").next().text().trim();
+							json.rarity = parseInt($('#unit-info-0').find("th:contains('Rarity')").next().children().children().length);
+							json.max_lvl = parseInt($('#unit-info-0').find("th:contains('Max Lv.')").next().text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.cost = parseInt($('#unit-info-0').find("th:contains('Cost')").next().text().trim().replace(/[^\d\.]/gi, ""), 10);
+							// Hit Counts
+							json.hits = parseInt($('#unit-info-0').find("th:contains('Normal')").next().text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.bb_hits = parseInt($('#unit-info-0').find("th:contains('Normal')").parent().next().children().first().next().text().trim().replace(/[^\d\.]/gi, ""), 10);
+							
+							// BB Fill
+							var bb_fill = parseInt($('#unit-info-0').find("th:contains('Normal')").parent().next().children().first().next().next().text().trim().replace(/[^\d\.]/gi, ""), 10);
+							// @TODO We need to split up 4 digits into XX ~ XX
+							// if (bb_fill.length > 2) {
+			
+							//} else {
+								json.bb_fill = parseInt($('#unit-info-0').find("th:contains('Normal')").parent().next().children().first().next().next().text().trim().replace(/[^\d\.]/gi, ""), 10);
+							// }
+							json.sbb_hits = parseInt($('#unit-info-0').find("th:contains('SBB')").next().text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.sbb_fill = parseInt($('#unit-info-0').find("th:contains('SBB')").next().next().text().trim().replace(/[^\d\.]/gi, ""), 10);
+
+							// Second wrapper (The Image)
+							var imageURL = $('#unit-info-1').find('a').first().attr('href');
+							// Remove extra query markup from image url
+							json.image = imageURL.substring(0, imageURL.indexOf('?')).replace('/revision/latest', '');
+
+							// Icon
+							var iconURL = $('a[title="' + json.name + '"]').find('img').first().attr('data-src');
+							if (typeof iconURL !== "undefined") {
+								json.icon = iconURL.substring(0, imageURL.indexOf('?')).replace('/revision/latest', '');
+							}
+							//console.log(json.icon);
+							//json.icon = $(icon).find('img').attr('src');
+							//console.log(json.icon);
+
+							// Third wrapper (Stats, BB/SBB Names, etc)
+							var statsTable = $('#unit-info-2');
+							// Base Stats
+							json.hp_base = parseInt($(statsTable).find('tr').eq(1).find('td').eq(0).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.atk_base = parseInt($(statsTable).find('tr').eq(1).find('td').eq(1).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.def_base = parseInt($(statsTable).find('tr').eq(1).find('td').eq(2).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.rec_base = parseInt($(statsTable).find('tr').eq(1).find('td').eq(3).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							// Lord Stats
+							json.hp_lord = parseInt($(statsTable).find('tr').eq(2).find('td').eq(0).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.atk_lord = parseInt($(statsTable).find('tr').eq(2).find('td').eq(1).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.def_lord = parseInt($(statsTable).find('tr').eq(2).find('td').eq(2).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.rec_lord = parseInt($(statsTable).find('tr').eq(2).find('td').eq(3).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							// Anima Stats
+							json.hp_anima = parseInt($(statsTable).find('tr').eq(3).find('td').eq(0).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.atk_anima = parseInt($(statsTable).find('tr').eq(3).find('td').eq(1).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.def_anima = parseInt($(statsTable).find('tr').eq(3).find('td').eq(2).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.rec_anima = parseInt($(statsTable).find('tr').eq(3).find('td').eq(3).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							// Breaker Stats
+							json.hp_breaker = parseInt($(statsTable).find('tr').eq(4).find('td').eq(0).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.atk_breaker = parseInt($(statsTable).find('tr').eq(4).find('td').eq(1).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.def_breaker = parseInt($(statsTable).find('tr').eq(4).find('td').eq(2).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.rec_breaker = parseInt($(statsTable).find('tr').eq(4).find('td').eq(3).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							// Guardian Stats
+							json.hp_guardian = parseInt($(statsTable).find('tr').eq(5).find('td').eq(0).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.atk_guardian = parseInt($(statsTable).find('tr').eq(5).find('td').eq(1).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.def_guardian = parseInt($(statsTable).find('tr').eq(5).find('td').eq(2).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.rec_guardian = parseInt($(statsTable).find('tr').eq(5).find('td').eq(3).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							// Oracle Stats
+							json.hp_oracle = parseInt($(statsTable).find('tr').eq(6).find('td').eq(0).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.atk_oracle = parseInt($(statsTable).find('tr').eq(6).find('td').eq(1).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.def_oracle = parseInt($(statsTable).find('tr').eq(6).find('td').eq(2).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							json.rec_oracle = parseInt($(statsTable).find('tr').eq(6).find('td').eq(3).text().trim().replace(/[^\d\.]/gi, ""), 10);
+
+							// Leader Skill
+							json.leader_skill_name = $(statsTable).find("b:contains('Leader Skill:')").text().replace('Leader Skill:', '').trim();
+							// BB Skill
+							json.bb_skill_name = $(statsTable).find("b:contains('Brave Burst:')").text().replace('Brave Burst:', '').trim();
+							// SBB Skill
+							json.sbb_skill_name = $(statsTable).find("b:contains('Super BB:')").text().replace('Super BB:', '').trim();
+							// Extract Span Text
+							getAbilityDetails($("b:contains('Leader Skill:')"));
+							getAbilityDetails($("b:contains('Brave Burst:')"));
+							getAbilityDetails($("b:contains('Super BB:')"));
+							json.leader_skill = $(statsTable).find("b:contains('Leader Skill:')").parent().siblings().text().trim();
+							json.bb_skill = $(statsTable).find("b:contains('Brave Burst:')").parent().siblings().text().trim();
+							json.sbb_skill = $(statsTable).find("b:contains('Super BB:')").parent().siblings().text().trim();
+
+							// Cost to Upgrade
+							json.evo_cost = parseInt($(statsTable).find("th:contains('Cost')").siblings().find('div').eq(0).text().trim().replace(/[^\d\.]/gi, ""), 10);
+							// Evolution Materials
+							json.evo_1 = $(statsTable).find("th:contains('Cost')").siblings().find('a').eq(0).attr('title');
+							json.evo_2 = $(statsTable).find("th:contains('Cost')").siblings().find('a').eq(1).attr('title');
+							json.evo_3 = $(statsTable).find("th:contains('Cost')").siblings().find('a').eq(2).attr('title');
+							json.evo_4 = $(statsTable).find("th:contains('Cost')").siblings().find('a').eq(3).attr('title');
+							json.evo_5 = $(statsTable).find("th:contains('Cost')").siblings().find('a').eq(4).attr('title');
+
+							// Series (we use the first unit to decide what the name of the series is).
+							var series = $(statsTable).find("th:contains('Chain')").siblings().find('a').eq(0).attr('title');
+							json.series = decodeURI(series);
+
+							// How to Obtain
+							var obtainList = $('#unit-info-3');
+							$(obtainList).find('li').each(function (index) {
+								var description = $(this).text().trim();
+								var obtainURL = 'http://bravefrontierglobal.wikia.com' + $(this).find('a').attr('href');
+								if (typeof $(this).find('a').attr('href') !== "undefined") {
+								json.how_to_obtain[index] = { how: description, url: obtainURL };
+								} else {
+									json.how_to_obtain[index] = { how: description };
+								}
+							});
+
+							// DATA cleanup
+							// If no evo material, set 'None' for that value.
+							if (typeof json.evo_1 == "undefined")  { json.evo_1 = 'None'; }
+							if (typeof json.evo_2 == "undefined")  { json.evo_2 = 'None'; }
+							if (typeof json.evo_3 == "undefined")  { json.evo_3 = 'None'; }
+							if (typeof json.evo_4 == "undefined")  { json.evo_4 = 'None'; }
+							if (typeof json.evo_5 == "undefined")  { json.evo_5 = 'None'; }
+
+							if (json.leader_skill_name == "")  { json.leader_skill_name = 'None'; }
+							if (json.leader_skill == "")  { json.leader_skill = 'None'; }
+							if (json.bb_skill_name == "")  { json.bb_skill_name = 'None'; }
+							if (json.bb_skill == "")  { json.bb_skill = 'None'; }
+							if (json.sbb_skill_name == "")  { json.sbb_skill_name = 'None'; }
+							if (json.sbb_skill == "")  { json.sbb_skill = 'None'; }
+		}
+
+					fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+						console.log('File successfully written! - Check your project directory for the output.json file');
+					})
+
+		res.send($.html())
+	});
+});
+
 
 app.listen('8081');
 console.log('Magic happens on port 8081');
